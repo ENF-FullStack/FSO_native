41d7f36cb385291e41388d389362e9578c45b5cc
VP Tervonen <elamshin@protonmail.com>
2022-11-11 20:56:29 +0200

    10.12 luotu

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-    uri: 'http://192.168.1.101:4000/graphql',
+    uri: `http://${Constants.manifest?.extra.apolloUri}:4000/graphql`,
 });
 
 const createApolloClient = () => {
