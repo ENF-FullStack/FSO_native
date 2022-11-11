2ac6bdc681e195ce12c3ad6062e48a2ae250bfa9
VP Tervonen <elamshin@protonmail.com>
2022-11-11 06:03:08 +0200

    10.7 luotu

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme'
 import AppBarHeader from './AppBarHeader';
@@ -14,8 +14,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
   <View style={styles.container}>
-    <AppBarHeader header={'Repositories'} link={'/'} />
-    <AppBarHeader header={'Sign in'} link={'/signin'} />
+    <ScrollView horizontal>
+      <AppBarHeader header={'Repositories'} link={'/'} />
+      <AppBarHeader header={'Sign in'} link={'/signin'} />
+    </ScrollView>
   </View>
   )
 };
