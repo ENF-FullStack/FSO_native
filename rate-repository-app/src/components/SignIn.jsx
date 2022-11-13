d41503bccf91090110dce75ae184f820f2be9f9f
VP Tervonen <elamshin@protonmail.com>
2022-11-13 02:00:18 +0200

    10.15 luotu

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,8 +1,10 @@
 import SignInForm from './SignInForm';
 import useSignIn from '../hooks/useSignIn';
+import { useNavigate } from 'react-router-native';
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  let navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const {username, password } = values;
@@ -10,6 +12,7 @@ const SignIn = () => {
     try {
       const { authenticate } = await signIn({ username, password })
       console.log('Auth results: ', authenticate);
+      navigate('/', { replace: true });
     } catch (error) {
       console.log(error);
     }
--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,13 +1,41 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
+
+const { apolloUri } = `http://${Constants.manifest?.extra}:4000/graphql`
 
 const httpLink = createHttpLink({
-    uri: `http://${Constants.manifest?.extra.apolloUri}:4000/graphql`,
+    // uri: `http://${Constants.manifest?.extra.apolloUri}:4000/graphql`,
+    uri: apolloUri,
 });
 
-const createApolloClient = () => {
+// const createApolloClient = () => {
+//     return new ApolloClient({
+//         link: httpLink,
+//         cache: new InMemoryCache(),
+//     });
+// };
+
+const createApolloClient = (authStorage) => {
+    const authLink = setContext(async (_, { headers }) => {
+        try {
+            const accessToken = await authStorage.getAccessToken();
+            return {
+                headers: {
+                    ...headers,
+                    authorization: accessToken ? `Bearer ${accessToken}` : '',
+                },
+            };
+        } catch (error) {
+            console.log(error);
+            return {
+                headers,
+            };
+        }
+    });
+
     return new ApolloClient({
-        link: httpLink,
+        link: authLink.concat(httpLink),
         cache: new InMemoryCache(),
     });
 };
